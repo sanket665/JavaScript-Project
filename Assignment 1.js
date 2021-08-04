bc6f7a13e96d0ea75f8736b786743fee3f5ba3cb
sanket665 <87305704+sanket665@users.noreply.github.com>
2021-08-04 04:09:35 -0700

    Assignment 1 Submitted

---
+++ Assignment 1.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Assignment 1</title>   
+</head>
+<body>
+      
+        <h1>Arithmetic Operations</h1>
+	  
+       <script>
+         let a =10;
+         let b =20;
+		
+         let add,sub,mult,div;
+		
+         add=a+b;
+         sub=a-b;
+         mult=a*b;
+         div=a/b;
+		
+         console.log(`The addition of ${a} and ${b} is ${add}`); 
+         console.log(`The subtraction of ${a} and ${b} is ${sub}`); 
+         console.log(`The multiplication of ${a} and ${b} is ${mult}`); 
+         console.log(`The division of ${a} and ${b} is ${div}`); 
+		
+       </script>
+</body>
+</html>
